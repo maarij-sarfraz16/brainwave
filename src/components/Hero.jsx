aeebee0a9b39e174c09bcc97950b607ea7b1dcfc
Maarij Sarfraz <maarijsarfraz13@gmail.com>
2024-10-11 16:51:29 +0500

    Hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,16 @@ import Button from "./Button";
 import curve from "../assets/hero/curve.png";
 import robot from "../assets/hero/robot.jpg";
 import heroBackground from "../assets/hero/hero-background.jpg";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/design/Hero";
+import {heroIcons} from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]" 
@@ -13,10 +21,11 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with 
+            Explore the Possibilities of &nbsp;AI&nbsp;Chatting with 
+            {` `}
             <span className="inline-block relative">
                 Brainwave
               <img
@@ -41,28 +50,67 @@ const Hero = () => {
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
-              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]"> {/* Fixed typo here */}
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]"> 
                 <img 
                   src={robot}
-                  className="w-full"
+                  className="w-full scale-[1.7] translate-y-[8%]
+                  md:scale-[1] md:translate-y-[10%] lg:-translate-y-[23%]"
                   width={1024}
                   height={490}
                   alt="AI"
                 />
+             
+                <Generating className="absolute left-4 
+                right-4 bottom-5 md:left-1/2 
+                md:right-auto md:bottom-8 md:w-[31rem]
+                md:-translate-x-1/2"/>
+                
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem]
+                  bottom-[7.5rem] px-1 py-1 bg-n-9/40
+                  backdrop-blue border border-n-1/10
+                  rounded-2xl xl:flex">
+                     {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img 
+                          src={icon}
+                          width={24}
+                          height={25}
+                          alt={`Icon ${index + 1}`} // More meaningful alt attribute
+                        />
+                      </li>
+                     ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem]
+                    bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
-          <div cla>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img 
               src={heroBackground}
               className="w-full"
               width={1440}
               height={1800}
-              alt="hero"
+              alt="Hero background"
             />
           </div>
+          <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="hidden relative z-10
+        mt-20 lg:block"/>
       </div>
+      <BottomLine />
     </Section>
   );
 }; 
